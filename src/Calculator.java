33a796c20342bfd2796a5a5535d6a9c31ed3de9b
relytnroh <relytnroh@gmail.com>
2022-01-05 09:50:54 -0800

    calculator

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+package src;
+
+public class Calculator {
+
+    public Calculator() {
+
+    }
+
+    public float add(float firstNumber, float secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public float substract(float firstNumber, float secondNumber){
+        return firstNumber - secondNumber;
+    }
+
+    public float divide(float firstNumber, float secondNumber){
+        return firstNumber / secondNumber;
+    }
+
+    public float multiply(float firstNumber, float secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+    public float getRemainder(float firstNumber, float secondNumber){
+        return firstNumber % secondNumber;
+    }
+
+}
