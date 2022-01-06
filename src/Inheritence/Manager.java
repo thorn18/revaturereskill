cfb844f56415c5243de2cdefa449af93ddf318c7
relytnroh <relytnroh@gmail.com>
2022-01-06 08:57:27 -0800

    inheritence

--- src/Inheritence/Manager.java
+++ src/Inheritence/Manager.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Manager extends Supervisor{
 
-    public List<String> skillset;
+    public List<String> qualification;
 
     public Manager(String id, String name, String experience, List<String> skillset) {
         super(id, name, experience);
-        this.skillset = skillset;
+        this.qualification = skillset;
     }
 }
--- src/Inheritence/Supervisor.java
+++ src/Inheritence/Supervisor.java
@@ -1,16 +1,16 @@
 package src.Inheritence;
 
 public class Supervisor extends Employee{
-    public String getExperience() {
+    public int getExperience() {
         return experience;
     }
 
-    public void setExperience(String experience) {
+    public void setExperience(int experience) {
         this.experience = experience;
     }
 
-    public String experience;
-    public Supervisor(String id, String name, String experience) {
+    public int experience;
+    public Supervisor(String id, String name, int experience) {
         super(id,name);
         this.experience = experience;
 
